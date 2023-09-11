f1bcea5ff8061e51e1a759114b42be7c2b98fdbb
JuanLariasMendez <jlariasm@miumg.edu.gt>
2023-09-10 22:04:29 -0600

    Se implemento el uso de try/catch with resourses en todos los métodos del CRUD, de esta forma nos evitamos estar cerrando cada conexion que solicitamos al JDBC.

--- src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -12,10 +12,12 @@ public class ProductoController {
 
 	public int modificar(String nombre, String descripcion, Integer cantidad, Integer id) throws SQLException {
 		//Instancia la conexion para hacer posible el qyery/statement
-		Connection con = new ConnectionFactory().recuperaConexion();
+		ConnectionFactory factory = new ConnectionFactory();
+		final Connection con = factory.recuperaConexion();
 
-		//------------CONCATENANDO LOS VALORES EN LA QUERY---------------
-		//Creación del statemnt/query
+		try(con) {
+			//------------CONCATENANDO LOS VALORES EN LA QUERY---------------
+			//Creación del statemnt/query
 		/*Statement statement = con.createStatement();
 		statement.execute("UPDATE PRODUCTO SET "
 				+ " NOMBRE = '" + nombre + "'"
@@ -23,57 +25,97 @@ public class ProductoController {
 				+ ", CANTIDAD = " + cantidad
 				+ " WHERE ID = " + id);*/
 
-		//-------PASANDO LA RERPONSABILDIAD DE VER LOS VALORES AL JDBC-----------
-		PreparedStatement statement = con.prepareStatement("UPDATE PRODUCTO SET "
-				+ " NOMBRE = ?"
-				+ ", DESCRIPCION = ?"
-				+ ", CANTIDAD = ?"
-				+ " WHERE ID = ?" );
-		statement.setString(1,nombre);
-		statement.setString(2,descripcion);
-		statement.setInt(3,cantidad);
-		statement.setInt(4,id);
+			//-------PASANDO LA RERPONSABILDIAD DE VER LOS VALORES AL JDBC-----------
+			final PreparedStatement statement = con.prepareStatement("UPDATE PRODUCTO SET "
+					+ " NOMBRE = ?"
+					+ ", DESCRIPCION = ?"
+					+ ", CANTIDAD = ?"
+					+ " WHERE ID = ?" );
+			try(statement) {
+				statement.setString(1,nombre);
+				statement.setString(2,descripcion);
+				statement.setInt(3,cantidad);
+				statement.setInt(4,id);
 
-		statement.execute();
+				statement.execute();
+				int updateCount = statement.getUpdateCount();
 
-
-		int updateCount = statement.getUpdateCount();
-
-		con.close();
-
-		//Para corroborar que el elemento han sido actualizados
-		//Nos devuelve cuantas filas fueron modificadas luego de hacer el query dentro del statement
-		//Con retur, el programa nos da el mensaje, esto para no hacer un sout
-		return updateCount;
+				//con.close();
+				//Para corroborar que el elemento han sido actualizados
+				//Nos devuelve cuantas filas fueron modificadas luego de hacer el query dentro del statement
+				//Con retur, el programa nos da el mensaje, esto para no hacer un sout
+				return updateCount;
+			}
+		}
 	}
 
 	public int /*void*/ eliminar(Integer id) throws SQLException {
 		//Instancia la conexion para hacer posible el qyery/statement
-		Connection con = new ConnectionFactory().recuperaConexion();
+		final Connection con = new ConnectionFactory().recuperaConexion();
 
-		//------------CONCATENANDO LOS VALORES EN LA QUERY---------------
-		//Creación del statemnt/query
-		/*Statement statement = con.createStatement();
-		statement.execute("DELETE FROM PRODUCTO WHERE ID = " + id);*/
+		try(con) {
+			//------------CONCATENANDO LOS VALORES EN LA QUERY---------------
+			//Creación del statemnt/query
+			/*Statement statement = con.createStatement();
+			statement.execute("DELETE FROM PRODUCTO WHERE ID = " + id);*/
 
-		//-------PASANDO LA RERPONSABILDIAD DE VER LOS VALORES AL JDBC-----------
-		PreparedStatement statement = con.prepareStatement("DELETE FROM PRODUCTO WHERE ID = ?");
-		statement.setInt(1, id);
-		statement.execute();
+			//-------PASANDO LA RERPONSABILDIAD DE VER LOS VALORES AL JDBC-----------
+			final PreparedStatement statement = con.prepareStatement("DELETE FROM PRODUCTO WHERE ID = ?");
 
+			try(statement) {
+				statement.setInt(1, id);
+				statement.execute();
 
-		//Para corroborar que el elemento haya sido eliminado
-		//Nos devuelve cuantas filas fueron modificadas luego de hacer el query dentro del statement
-		//Al asignar el mensaje a una variable, tenemos que hacer un sout
-		//int updateCount = statement.getUpdateCount();
-		//Con retur, el programa nos da el mensaje, esto para no hacer un sout
-		return statement.getUpdateCount();
+				//Para corroborar que el elemento haya sido eliminado
+				//Nos devuelve cuantas filas fueron modificadas luego de hacer el query dentro del statement
+				//Al asignar el mensaje a una variable, tenemos que hacer un sout
+				//int updateCount = statement.getUpdateCount();
+				//Con retur, el programa nos da el mensaje, esto para no hacer un sout
+
+				return statement.getUpdateCount();
+				//con.close();
+			}
+		}
 	}
 
 	public List<Map<String, String>> listar() throws SQLException {
-
 		//Instancia la conexion para hacer posible el qyery/statement
-		Connection con = new ConnectionFactory().recuperaConexion();
+		final Connection con = new ConnectionFactory().recuperaConexion();
+
+		try(con) {
+			//-------PASANDO LA RERPONSABILDIAD DE VER LOS VALORES AL JDBC-----------
+			final PreparedStatement statement = con.prepareStatement("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+
+			try(statement) {
+				statement.execute();
+
+				//tomando el resultado del statement/query select
+				ResultSet resultSet = statement.getResultSet();
+				//Para ver todos los resultados, este tendría que ir fila por fila en la tabla productos, hasta llegar al ultimo
+				List<Map<String, String>> resultado = new ArrayList<>();
+
+				//Para ir ingresando un valor abajo de un valor, hacemos un recorrido de toda la tabla, valor por valor
+				while (resultSet.next()){
+					Map<String, String> fila = new HashMap<>();
+					//Como el mapa establecido es tipo String, con los valores int, debemos de convertirlos
+					fila.put("ID", String.valueOf (resultSet.getInt("ID")));//Para mostrar los ID ingresados
+					fila.put("NOMBRE",resultSet.getString("NOMBRE"));//Para mostrar los NOMBRES ingresados
+					fila.put("DESCRIPCION",resultSet.getString("DESCRIPCION"));//Para mostrar las DESCRIPCIONES ingresadas
+					// Como el mapa establecido es tipo String, con los valores int, debemos de convertirlos
+					fila.put("CANTIDAD", String.valueOf (resultSet.getInt("CANTIDAD")));//Para mostrar las CANTIDADES ingresadas
+
+					resultado.add(fila);//cada resultado lo vamos agregando a las filas
+					/*
+					//Para las columnas int, numericos, tenemos el siguiente método
+					resultSet.getInt("ID");//Este método nos devuelve la parte numerica de los campos numericos que tengamos
+					 */
+				}
+				return resultado;
+				//System.out.println(result);
+				//con.close();
+			}
+		}
+
 
 		//Para establecer conexion con mysql
 /*		Connection con = DriverManager.getConnection(
@@ -87,47 +129,6 @@ public class ProductoController {
 		//Devuelve un boolean debido a que indica que el statemnt si el resultado es un listado = true, si se ejecuta cualquier otro query este devuelve false
 		*//*boolean result =*//* statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
 		*/
-
-		//-------PASANDO LA RERPONSABILDIAD DE VER LOS VALORES AL JDBC-----------
-		PreparedStatement statement = con.prepareStatement("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
-		statement.execute();
-
-
-		//tomando el resultado del statement/query select
-		ResultSet resultSet = statement.getResultSet();
-		//Para ver todos los resultados, este tendría que ir fila por fila en la tabla productos, hasta llegar al ultimo
-		List<Map<String, String>> resultado = new ArrayList<>();
-
-		//Para ir ingresando un valor abajo de un valor, hacemos un recorrido de toda la tabla, valor por valor
-		while (resultSet.next()){
-			Map<String, String> fila = new HashMap<>();
-
-			//Para mostrar los ID ingresados
-			//Como el mapa establecido es tipo String, con los valores int, debemos de convertirlos
-			fila.put("ID", String.valueOf (resultSet.getInt("ID")));
-
-			//Para mostrar los NOMBRES ingresados
-			fila.put("NOMBRE",resultSet.getString("NOMBRE"));
-
-			//Para mostrar las DESCRIPCIONES ingresadas
-			fila.put("DESCRIPCION",resultSet.getString("DESCRIPCION"));
-
-			//Para mostrar las CANTIDADES ingresadas
-			// Como el mapa establecido es tipo String, con los valores int, debemos de convertirlos
-			fila.put("CANTIDAD", String.valueOf (resultSet.getInt("CANTIDAD")));
-
-			//cada resultado lo vamos agregando a las filas
-			resultado.add(fila);
-
-			/*
-			//Para las columnas int, numericos, tenemos el siguiente método
-			resultSet.getInt("ID");//Este método nos devuelve la parte numerica de los campos numericos que tengamos
-			 */
-		}
-
-		//System.out.println(result);
-		con.close();
-		return resultado;
 	}
 
     public void guardar(Map<String,String> producto) throws SQLException {
@@ -140,50 +141,49 @@ public class ProductoController {
 		//Se habre la conexion
 		ConnectionFactory factory = new ConnectionFactory();
 		//Se recupera la conexion
-		Connection con = factory.recuperaConexion();
-		//Tomando el control de la transaccion, para decir cuando se realiza el commit
-		con.setAutoCommit(false);
+		final Connection con = factory.recuperaConexion();
+		try(con){
+			//Tomando el control de la transaccion, para decir cuando se realiza el commit
+			con.setAutoCommit(false);
 
-
-		//Creado un statement, y concatenango los valores
+			//Creado un statement, y concatenango los valores
 			/*Statement statement = con.createStatement();
 			statement.execute("INSERT INTO PRODUCTO(nombre, descripcion, cantidad) "
 			+ " VALUES('" +producto.get("NOMBRE")+ "', '"
 			+ producto.get("DESCRIPCION")+ "', "
 			+ producto.get("CANTIDAD") + ")", Statement.RETURN_GENERATED_KEYS);*/
 
-		//Preparando un statemen para pasar la responsabilidad a SQL de validar la query
-		PreparedStatement statement = con.prepareStatement("INSERT INTO PRODUCTO"
-						+ "(nombre, descripcion, cantidad)"
-						+ " VALUES(?,?,?)",
-				Statement.RETURN_GENERATED_KEYS);
+			//Preparando un statemen para pasar la responsabilidad a SQL de validar la query
+			final PreparedStatement statement = con.prepareStatement("INSERT INTO PRODUCTO"
+							+ "(nombre, descripcion, cantidad)"
+							+ " VALUES(?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+			//try(statement){
+				//Si sale la transaccion se efectua, tenemos un commit y si ocurre un error, tenemos un rollback de la transacción.
+				//si vamos a estar ejecutando y tomando siempre el menor valor posible para ir guardando y pensando que el máximo que podemos guardar es 50, entonces si la cantidad es mayor que 50, la próxima vez que pasemos por este lazo, por este loop, nosotros tenemos que guardar lo restante.
+			try(statement){
+					//si la ejecución acá tiene un error, él ejecuta registro o cualquier cosa que hay acá dentro del try tiene un error, vamos a caer en el catch, nosotros vamos a hacer un rollback de la transacción, vamos a cerrar la conexión y no hay ningún problema. Nosotros cancelamos la ejecución de estas transacciones.
+					do{
+						// si es la cantidad tiene valor 100 y máximoCantidad es 50, el valor mínimo acá va a ser 50. Si el valor cantidad es 40 por ejemplo y el máximoCantidad es 50, el valor de cantidad para guardar va a ser 40.
+						int cantidadParaGuardar = Math.min(cantidad,maximoCantidad);
 
-		//Si sale la transaccion se efectua, tenemos un commit y si ocurre un error, tenemos un rollback de la transacción.
-		//si vamos a estar ejecutando y tomando siempre el menor valor posible para ir guardando y pensando que el máximo que podemos guardar es 50, entonces si la cantidad es mayor que 50, la próxima vez que pasemos por este lazo, por este loop, nosotros tenemos que guardar lo restante.
-		try{
-			//si la ejecución acá tiene un error, él ejecuta registro o cualquier cosa que hay acá dentro del try tiene un error, vamos a caer en el catch, nosotros vamos a hacer un rollback de la transacción, vamos a cerrar la conexión y no hay ningún problema. Nosotros cancelamos la ejecución de estas transacciones.
-			do{
-				// si es la cantidad tiene valor 100 y máximoCantidad es 50, el valor mínimo acá va a ser 50. Si el valor cantidad es 40 por ejemplo y el máximoCantidad es 50, el valor de cantidad para guardar va a ser 40.
-				int cantidadParaGuardar = Math.min(cantidad,maximoCantidad);
+						EjecutaReguistro(statement, nombre, descripcion, cantidadParaGuardar);
 
-				EjecutaReguistro(statement, nombre, descripcion, cantidadParaGuardar);
+						//Acá voy a estar haciendo una substracción del valor de cantidad del máximoCantidad.
+						cantidad -= maximoCantidad;
+						//Mientras cantidad sea mayor a cero, entonces sigue haciendo el loop
+					}while (cantidad>0);
 
-				//Acá voy a estar haciendo una substracción del valor de cantidad del máximoCantidad.
-				cantidad -= maximoCantidad;
-				//Mientras cantidad sea mayor a cero, entonces sigue haciendo el loop
-			}while (cantidad>0);
+					con.commit();
+					System.out.println("COMMIT");
 
-			con.commit();
-			System.out.println("COMMIT");
-		}catch (Exception e){
-			System.out.println("ROLLBACK");
-			con.rollback();
+				}catch (Exception e){
+				System.out.println("ROLLBACK");
+				con.rollback();
+			}
 		}
-
-		statement.close();
-
-		con.close();
-
+			//statement.close(); //Ya no es necesario debido a que utilizamos try/catch whit resource, y este método ya trae el close por defecto
+			//con.close(); //Ya no es necesario debido a que utilizamos try/catch whit resource, y este método ya trae el close por defecto
 	}
 
 	private static void EjecutaReguistro(PreparedStatement statement, String nombre, String descripcion, Integer cantidad) throws SQLException {
@@ -198,16 +198,33 @@ public class ProductoController {
 		statement.setInt(3, cantidad);
 		statement.execute();
 
-		ResultSet resultSet = statement.getGeneratedKeys(); //En esta variable tenemos el listado de IDs que fueron generados
+		/*-----try catch whitch resourese------*/
 
-		//Con este loop, podemos listar el listado de IDs que fue generado
-		while (resultSet.next()) {
-			System.out.println(
-					String.format(
-							"Fue insertado el producto de ID:%d",
-							resultSet.getInt(1)));
+		//---------Forma de java 7 para atras-----------
+		//Para que el JDBC se encargue de cerrar los stetemts por su cuenta y no tener que hacerlo manualmente en cada parte del código
+		/*try(ResultSet resultSet = statement.getGeneratedKeys();)*//*En esta variable tenemos el listado de IDs que fueron generados)*//*{
+			//Con este loop, podemos listar el listado de IDs que fue generado
+			while (resultSet.next()) {
+				System.out.println(
+						String.format(
+								"Fue insertado el producto de ID:%d",
+								resultSet.getInt(1)));
+			}
+		}*/
 
+
+		//---------Forma de java 9 en adelante-----------
+		final ResultSet resultSet = statement.getGeneratedKeys();
+		try(resultSet)/*En esta variable tenemos el listado de IDs que fueron generados)*/{
+			//Con este loop, podemos listar el listado de IDs que fue generado
+			while (resultSet.next()) {
+				System.out.println(
+						String.format(
+								"Fue insertado el producto de ID:%d",
+								resultSet.getInt(1)));
+			}
 		}
+
 	}
 }
 
