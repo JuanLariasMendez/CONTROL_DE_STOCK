5c3f8870501255057df1ae3eae203737e7b6deb2
JuanLariasMendez <jlariasm@miumg.edu.gt>
2023-09-13 13:35:25 -0600

    Se implemento el modelo y DAO de la tabla categoria de la base de datos, ademas fue desarrollado lógica necesaria en el producto controller para que hiciera puente entre el model y sea visible en la view. AUnque por el momento al guardar un nuevo producto desde la view, guarda como null la referencia del categoria

--- src/main/java/com/alura/jdbc/controller/CategoriaController.java
+++ src/main/java/com/alura/jdbc/controller/CategoriaController.java
@@ -1,13 +1,23 @@
 package com.alura.jdbc.controller;
 
+import com.alura.jdbc.dao.CategoriaDAO;
+import com.alura.jdbc.factory.ConnectionFactory;
+import com.alura.jdbc.modelo.Categoria;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class CategoriaController {
+    private CategoriaDAO categoriaDAO;
 
-	public List<?> listar() {
-		// TODO
-		return new ArrayList<>();
+    public CategoriaController(){
+        var factory = new ConnectionFactory();
+        this.categoriaDAO = new CategoriaDAO(factory.recuperaConexion()); //estamos inicializando el DAO enviando una conexión del pool de conexiones
+    }
+
+    //Listado unicamente para categoria
+	public List<Categoria> listar() {
+		return categoriaDAO.lista();
 	}
 
     public List<?> cargaReporte() {
